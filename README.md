94ccd3ae86dc5562e3b828dba8c6dc2ae5d794bb
Yoshi2889 <rick.2889@gmail.com>
2017-06-28 17:26:21 +0200

    Initial commit
    Signed-off-by: Yoshi2889 <rick.2889@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# collections
-Simple Collection class allowing storage of specific data.
+# Collections
+[![Build Status](https://scrutinizer-ci.com/g/Yoshi2889/collections/badges/build.png?b=3.0)](https://scrutinizer-ci.com/g/Yoshi2889/collections/build-status/master)
+[![Scrutinizer Code Quality](https://scrutinizer-ci.com/g/Yoshi2889/collections/badges/quality-score.png?b=3.0)](https://scrutinizer-ci.com/g/Yoshi2889/collections/?branch=master)
+[![Latest Stable Version](https://poser.pugx.org/yoshi2889/collections/v/stable)](https://packagist.org/packages/yoshi2889/collections)
+[![Latest Unstable Version](https://poser.pugx.org/yoshi2889/collections/v/unstable)](https://packagist.org/packages/yoshi2889/collections)
+[![Total Downloads](https://poser.pugx.org/yoshi2889/collections/downloads)](https://packagist.org/packages/yoshi2889/collections)
+
+Simple Collection class allowing storage of specific data, based on PHP's ArrayObject.
+
+## Installation
+You can install this class via `composer`:
+
+```composer require yoshi2889/collections```
+
+## Usage
+To use a Collection, create a new instance:
+
+```php
+$validationClosure = function ($value)
+{
+    return is_string($value);
+};
+
+$initialItems = ['This is a test value!', 'This is another test value!'];
+
+$collection = new \Yoshi2889\Collections\Collection($validationClosure, $initialItems);
+```
+
+Note that the closure passed as the first parameter to the `Collection` constructur **MUST** return a boolean value.
+This function is used to validate any added data types. A returned value of true means the given value may be added to the collection.
+
+The second parameter can contain any initial values which should be in the collection. These items will also be validated.
+
+## License
+This code is released under the MIT License. Please see `LICENSE` to read it.
---
+++ src/Collection.php
@@ -0,0 +1,112 @@
+<?php
+/**
+ * Copyright 2017 NanoSector
+ *
+ * You should have received a copy of the MIT license with the project.
+ * See the LICENSE file for more information.
+ */
+
+namespace Yoshi2889\Collections;
+
+class Collection extends \ArrayObject
+{
+	/**
+	 * @var \Closure
+	 */
+	protected $validator = '';
+
+	/**
+	 * Collection constructor.
+	 *
+	 * @param \Closure $valueValidator
+	 * @param array $initialValues
+	 *
+	 * @internal param string $expectedValueType
+	 */
+	public function __construct(\Closure $valueValidator, array $initialValues = [])
+	{
+		$this->validator = $valueValidator;
+
+		foreach ($initialValues as $key => $initialValue)
+			$this->offsetSet($key, $initialValue);
+	}
+
+	/**
+	 * @param $value
+	 */
+	public function append($value)
+	{
+		if (!$this->validateType($value))
+			throw new \InvalidArgumentException('Given value does not match expected value type for this collection.');
+
+		parent::append($value);
+	}
+
+	/**
+	 * @param mixed $value
+	 *
+	 * @return bool
+	 */
+	public function contains($value): bool
+	{
+		return in_array($value, (array) $this);
+	}
+
+	/**
+	 * @param $value
+	 *
+	 * @return false|int|string|mixed
+	 */
+	public function getOffset($value)
+	{
+		return array_search($value, (array) $this);
+	}
+
+	/**
+	 * @return array
+	 */
+	public function keys(): array
+	{
+		return array_keys((array) $this);
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function offsetSet($offset, $value)
+	{
+		if (!$this->validateType($value))
+			throw new \InvalidArgumentException('Given value does not match expected value type for this collection.');
+
+		parent::offsetSet($offset, $value);
+	}
+
+	/**
+	 * @param mixed $value
+	 */
+	public function remove($value)
+	{
+		if (!$this->contains($value))
+			throw new \InvalidArgumentException('The given value does not exist in this collection.');
+
+		$this->offsetUnset($this->getOffset($value));
+	}
+
+	/**
+	 * @param $value
+	 *
+	 * @return bool
+	 */
+	public function validateType($value): bool
+	{
+		return ($this->validator)($value);
+	}
+
+	/**
+	 * @return array
+	 */
+	public function values(): array
+	{
+		return array_values((array) $this);
+	}
+}
